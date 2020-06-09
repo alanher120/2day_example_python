5d94f5470802828b0919a44cc14b310ab2de52db
Abola Lee <abola921@gmail.com>
2020-06-09 17:06:02 +0800

    Create app.py

---
+++ src/app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask, request, jsonify, abort
+from waitress import serve
+
+# init Flask
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def show_pod_name():
+  return f"<body style='background-color:#009933;color:white;'><h1>{os.getenv('HOSTNAME')}</h1></body>"
+  
+# run Flask Restful service
+serve(app, port=3000, host='0.0.0.0')
