2df43a5fb09c4cc27e1bd5810c3610862a76e535
Abola Lee <abola921@gmail.com>
2020-06-09 17:34:12 +0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,13 +1,19 @@
 import os
-from flask import Flask, request, jsonify, abort
+from flask import Flask
 from waitress import serve
 
 # init Flask
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def show_pod_name():
-  return f"<body style='background-color:#009933;color:white;'><h1>{os.getenv('HOSTNAME')}</h1></body>"
-  
+  return f"""
+            <body style='background-color:#009933;color:white;'>
+              <h1>{os.getenv('HOSTNAME')}</h1>
+            </body>
+         """
+
+
 # run Flask Restful service
 serve(app, port=3000, host='0.0.0.0')
